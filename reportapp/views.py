c58a8d715eda687dbeec32ce9d4fe9b781398a80
volentib <volentib@student.21-school.ru>
2025-09-10 09:59:02 +0500

    done

--- reportapp/views.py
+++ reportapp/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from django.db.models import Count, Avg, Sum, Max, Q
-from django.db.models.functions import Round 
+from django.db.models import Count, Sum, Max, Q
+from django.db.models.functions import Round
 
 from .models import TaxiTrip
 
@@ -10,12 +10,18 @@ from .models import TaxiTrip
 class Report(APIView):
     def get(self, request):
         stats = TaxiTrip.objects.filter(trip_distance__gt=0).aggregate(
-            short_trips=Count('trip_distance', filter=Q(trip_distance__lt=1)),
-            medium_trips=Count('trip_distance', filter=Q(trip_distance__gte=1, trip_distance__lt=5)),
-            long_trips=Count('trip_distance', filter=Q(trip_distance__gte=5, trip_distance__lt=10)),
-            very_long_trips=Count('trip_distance', filter=Q(trip_distance__gte=10)),           
-            avg_distance=Round(Avg('trip_distance'), 2), 
-            max_distance=Max('trip_distance'),
-            total_distance=Round(Sum('trip_distance'), 2) 
+            short_trips=Count("trip_distance", filter=Q(trip_distance__lt=1)),
+            medium_trips=Count("trip_distance", filter=Q(trip_distance__gte=1, trip_distance__lt=5)),
+            long_trips=Count("trip_distance", filter=Q(trip_distance__gte=5, trip_distance__lt=10)),
+            very_long_trips=Count("trip_distance", filter=Q(trip_distance__gte=10)),
+            max_distance=Max("trip_distance"),
+            total_distance=Round(Sum("trip_distance"), 2),
         )
+
+        stats["avg_distance"] = (round(stats["total_distance"] / (stats["short_trips"]
+                                                                + stats["medium_trips"]
+                                                                + stats["long_trips"]
+                                                                + stats["very_long_trips"]
+                                                            ), 2))
+
         return Response(stats)
